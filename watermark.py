573f969116f889a8fe323eb8d4a6f84ec2afac32
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-12-19 10:28:44 +0530

    Update watermark.py

--- watermark.py
+++ watermark.py
@@ -1,13 +1,16 @@
 import subprocess
 
-def watermark_video(inp, out, text, logo, scale, position):
+def watermark_video(input_video, output_video, text, logo, scale, position):
+
     pos = {
         "bottom-center": "x=(w-text_w)/2:y=h-text_h-20",
         "bottom-left": "x=20:y=h-text_h-20",
         "bottom-right": "x=w-text_w-20:y=h-text_h-20",
+
         "top-left": "x=20:y=20",
         "top-center": "x=(w-text_w)/2:y=20",
         "top-right": "x=w-text_w-20:y=20",
+
         "middle-left": "x=20:y=(h-text_h)/2",
         "center": "x=(w-text_w)/2:y=(h-text_h)/2",
         "middle-right": "x=w-text_w-20:y=(h-text_h)/2"
@@ -17,22 +20,20 @@ def watermark_video(inp, out, text, logo, scale, position):
 
     cmd = [
         "ffmpeg", "-y",
-        "-i", inp,
+        "-i", input_video,
         "-i", logo,
         "-filter_complex",
-        f"""
-        [1:v]scale=iw*{scale}/100:-1[wm];
-        [0:v][wm]overlay=W-w-20:H-h-20,
-        drawtext=text='{text}':
-        fontcolor=white@0.85:
-        fontsize=24:
-        {draw_pos}
-        """ ,
+        f"[1:v]scale=iw*{scale}/100:-1[wm];"
+        f"[0:v][wm]overlay=W-w-20:H-h-20,"
+        f"drawtext=text='{text}':"
+        f"fontcolor=white@0.85:"
+        f"fontsize=24:"
+        f"{draw_pos}",
         "-c:v", "libx264",
         "-preset", "fast",
         "-c:a", "copy",
         "-movflags", "+faststart",
-        out
+        output_video
     ]
 
     subprocess.run(cmd, check=True)
