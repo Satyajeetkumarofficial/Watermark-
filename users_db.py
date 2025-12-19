d99b116d6b53968a87dad8fa24c1508cbd4b1154
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-12-19 10:27:44 +0530

    Update users_db.py

--- users_db.py
+++ users_db.py
@@ -1,13 +1,19 @@
 users = {}
+stats = {
+    "users": set(),
+    "processed": 0
+}
 
 def get_user(uid):
+    stats["users"].add(uid)
+
     if uid not in users:
         users[uid] = {
             "rename": None,
             "thumb": None,
-            "scale": 20,
+            "scale": DEFAULT_SCALE if "DEFAULT_SCALE" in globals() else 20,
             "position": "bottom-center",
             "awaiting": None,
-            "last": None
+            "last_video_msg": None
         }
     return users[uid]
