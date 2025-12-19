6a64ead053c8c3e582f3ac210b23c1ea5f89b828
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-12-19 10:41:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,19 @@ from bans_db import is_banned, ban, unban
 from watermark import watermark_video
 import os
 from datetime import datetime
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive", 200
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
 app = Client(
     "watermark_bot",
